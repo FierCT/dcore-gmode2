d583b7e646ffe53124f50a91ac0cefbe3c0c786b
FierDaGamerZ <fierthegamer@gmail.com>
2018-02-09 14:39:46 -0500

    Rewrote sh_dcore.lua
    As a part of the rewrite to make this project easier to manage: began changing how we send and work with data for drawing the Derma and how we handle Paint, accepting/denying requests. Not finished - will continue another time.

--- dcore/lua/autorun/sh_dcore.lua
+++ dcore/lua/autorun/sh_dcore.lua
@@ -2,97 +2,82 @@ if SERVER then
   AddCSLuaFile()
 end
 
-local BoxOpen = false
-local _DRAllowed = {}
-local _DRBlocked = {}
+local _DermaCore = {}
+_DermaCore["requests"] = {}
+_DermaCore["requests"]["isOpen"] = false
+_DermaCore["requests"]["instances"] = {}
+-- _DermaCore["requests"]["instances"][entity ???] <= 0 or >= 1
+-- 0 is denied
+-- 1 is allowed
+_DermaCore["derma"] = {}
 
-local DermaFrames = {}
-local DermaID = {}
+net.Receive("f_drequest", function()
+  local sender = net.ReadEntity()
+  local chip = net.ReadEntity()
+  local id = net.ReadFloat()
 
-local AD = AllDerma
-
-local function requestSh(ply, id, chip)
-  if not IsValid(ply) then return end
-  if not ply:IsPlayer() then return end
-
-  if not chip then return end
-  if not IsValid(chip) then return end
-
-  if BoxOpen then return end
-
-  surface.PlaySound("plats/elevbell1.wav")
-  BoxOpen = true
-
-  local w = vgui.Create("DFrame")
-  w:SetTitle("Derma request from "..ply:Name())
-  w:SetBackgroundBlur(true)
-
-  w:SetSize(300,119)
-  w:Center()
-  w:ShowCloseButton(false)
-  w:MakePopup()
-
-  local a = vgui.Create("AvatarImage",w)
-  a:SetPos(7,32)
-  a:SetSize(64,64)
-  a:SetPlayer(ply,64)
-
-  local t = vgui.Create("DLabel", w)
-  t:SetPos(78,32)
-  t:SetText(ply:Name().."'s E2 is asking for permission to use Derma\nWould you like to accept?")
+  if _DermaCore["requests"]["instances"][sender:SteamID()] <= 0 then return end
+  if not IsValid(sender) or not sender:IsPlayer() then return end
+  if not IsValid(chip) or not chip then return end
+  
+  if _DermaCore["requests"]["isOpen"] then return end
+  
+  --surface.PlaySound("plats/elevbell1.wav")
+  local f = vgui.Create("DFrame")
+  f:SetBackgroundBlur(true)
+  f:SetSize(ScrW()/3, ScrH()/8) -- 1920/3 = 640, 1080/8 = 135...size for most players: 640, 135
+  f:Center()
+  f:ShowCloseButton(false)
+  f:MakePopup()
+  
+  local a = vgui.Create("AvatarImage", f)
+  a:SetPos(6, 32)
+  a:SetSize(64, 64)
+  a:SetPlayer(sender, 64)
+  
+  local t = vgui.Create("DLabel", f)
+  t:SetPos(76, 32)
+  t:SetText(sender:Name().."'s Expression 2 Chip is asking for permission to draw Derma.\nWould you like to accept?")
   t:SizeToContents()
   t:SetWrap(true)
-
-  local y = vgui.Create("DButton", w)
-  y:SetPos(80,64)
-  y:SetSize(100,32)
-  y:SetText("Accept")
-  y.DoClick = function()
-    if !y:GetDisabled() then
+  
+  local yes = vgui.Create("DButton", f)
+  yes:SetPos(80, 64)
+  yes:SetSize(100, 32)
+  yes:SetText("Accept")
+  yes.DoClick = function()
+    if not yes:GetDisabled() then
       RunConsoleCommand("de2_accept", id)
-      _DRAllowed[chip] = true
-      BoxOpen = false
-      w:Remove()
+      _DermaCore["requests"]["instances"][chip] = 1
+      _DermaCore["requests"]["isOpen"] = false
+      f:Remove()
     end
   end
-
-  local n = vgui.Create("DButton", w)
-  n:SetPos(190,64)
-  n:SetSize(100,32)
-  n:SetText("Deny")
-  n.DoClick = function()
-    if y:GetDisabled() then
-      _DRBlocked[ply:SteamID()] = true
+  
+  local no = vgui.Create("DButton", f)
+  no:SetPos(190, 64)
+  no:SetSize(100, 32)
+  no:SetText("Deny")
+  no.DoClick = function()
+    if yes:GetDisabled() then
+      _DermaCore["requests"]["instances"][sender:SteamID()] = -1
     end
-      RunConsoleCommand("de2_deny", id)
-      _DRAllowed[chip] = false
-      BoxOpen = false
-      w:Remove()
+    RunConsoleCommand("de2_deny", id)
+    _DermaCore["requests"]["isOpen"] = false
+    f:Remove()
   end
-
-  local c = vgui.Create("DCheckBoxLabel", w)
-  c:SetPos(7, 99)
-  c:SetText("Block "..ply:Name().."'s derma requests?")
-  c:SetValue(0)
-  c:SizeToContents()
-  c.OnChange = function(self, value)
+  
+  local block = vgui.Create("DCheckBoxLabel", f)
+  block:SetPos(7, 99)
+  block:SetText("Block "..sender:Name().."'s Derma requests")
+  block:SetValue(0)
+  block:SizeToContents()
+  block.OnChange = function(self, value)
     if value then
       y:SetDisabled(true)
     else
       y:SetDisabled(false)
     end
-  end
-end
-
-net.Receive("f_drequest", function()
-  local ply = net.ReadEntity()
-  local chip = net.ReadEntity()
-  local id = net.ReadFloat()
-
-  if _DRBlocked[ply:SteamID()] then return end
-
-  if not _DRAllowed[chip] then
-    requestSh(ply, id, chip)
   end
 end)
 
@@ -123,129 +108,79 @@ end)
 
 -- where we're actually drawing derma! :D
 
-local DObjects = {}
-DObjects["frame"] = "frame"
-DObjects["text"] = "text"
-DObjects["button"] = "button"
-DObjects["textentry"] = "textentry"
+local DermaObj = {}
+DermaObj["DFrame"] = true
+
 
 net.Receive("f_drawderma", function()
-  local tab = net.ReadTable()
-  local chip = tab["chip"]
-  local id = tab["id"]
-  local typ = tab["type"]
-
-  if not _DRAllowed[chip] then return end
-  if not DObjects[typ] then return end
-
-  if typ == "frame" then
-
-    if DermaID[id] then return end
-
-    local x, y, w, h = tab["x"], tab["y"], tab["w"], tab["h"]
-    local title = tab["title"]
-    local drag = tab["draggable"]
-    local color = tab["color"]
-
-    local frame = vgui.Create("DFrame")
-    if x > ScrW() or x < 0 then x = ScrW()/2-w/2 end
-    if y > ScrH() or y < 0 then y = ScrH()/2-h/2 end
-    frame:SetPos(x, y)
-    if w > ScrW() then w = ScrW() end
-    if h > ScrH() then h = ScrH() end
-    if w < 200 then w = 200 end
-    if h < 75 then h = 75 end
-    frame:SetSize(w, h)
-    frame:SetTitle(title)
-    frame:SetDraggable(drag)
-    frame:SetDeleteOnClose(true)
-    frame:ShowCloseButton(true)
-
-    frame.OnClose =
-    function()
-      for k, v in pairs(DermaID) do
-        if DermaID[k]["parent"] == id then
-          DermaID[k] = nil
+  local dtype = net.ReadString()
+  local dsent = net.ReadTable()
+  
+  local inst = dsent["chip"] -- inst == chip
+  local id = dsent["id"] -- id of the object, is string, could be number
+  
+  if not dtype or DermaObj[dtype] then return end
+  if not _DermaCore["requests"]["instances"][inst] or _DermaCore["requests"]["instances"][inst] <= 0 then return end
+  
+  if dtype == "DFrame" then
+    if _DermaCore["derma"][id] then return end
+    
+    local pos = dsent["pos"]
+    local size = dsent["size"]
+    local canDrag = dsent["draggable"]
+    local color = dsent["color"]
+    local title = dsent["title"]
+    
+    size[1] = math.Clamp(size[1], 800, ScrW())
+    size[2] = math.Clamp(size[2], 600, ScrH())
+    
+    pos[1] = math.Clamp(pos[1], 0, ScrW()-size[1])
+    pos[2] = math.Clamp(pos[2], 0, ScrH()-size[2])
+    
+    local f = vgui.Create("DFrame")
+    f:SetPos(pos[1], pos[2])
+    f:SetSize(size[1], size[2])
+    f:SetTitle(title)
+    f:SetDraggable(canDrag)
+    f:SetDeleteOnClose(true)
+    f:ShowCloseButton(true)
+    
+    f.OnClose = function()
+      for k, v in pairs(_DermaCore["derma"]) do
+        if _DermaCore["derma"][k]["parent"] == id then
+          _DermaCore["derma"][k] = nil
         end
       end
-      DermaFrames[id] = nil
+      _DermaCore["derma"][id] = nil
     end
-
-    frame:SetSizable(false)
-    frame:MakePopup()
-
-    DermaFrames[id] = frame
-    DermaID[id] = {}
-    DermaID[id]["parent"] = id
-    DermaID[id]["obj"] = frame
-
-  elseif typ == "text" then
-
-    if DermaID[id] then return end
-
-    local x, y = tab["x"], tab["y"]
-    local text2 = tab["text"]
-    local parent = tab["parent"]
+    
+    f:SetSizable(false)
+    f:MakePopup()
+    
+    _DermaCore["derma"][id] = {}
+    _DermaCore["derma"][id]["parent"] = id
+    _DermaCore["derma"][id]["obj"] = f
+    
+  elseif dtype == "DText" then
+    if _DermaCore["derma"][id] then return end
+    
+    local pos = dsent["pos"]
+    local text = dsent["text"]
+    local parent = dsent["parent"]
     local color = tab["color"]
-
-    local text = vgui.Create("DLabel", DermaFrames[parent])
-    text:SetPos(x,y)
-    text:SetText(text2)
-    text:SizeToContents()
-    text:SetTextColor(Color(color[1],color[2],color[3]))
-
-    DermaID[id] = {}
-    DermaID[id]["parent"] = parent
-    DermaID[id]["obj"] = text
-
-  elseif typ == "button" then
-    local x, y, w, h = tab["x"], tab["y"], tab["w"], tab["h"]
-    local text = tab["text"]
-    local parent = tab["parent"]
-    local color = tab["color"]
-
-    local button = vgui.Create("DButton", DermaFrames[tab["parent"]])
-    button:SetPos(x,y)
-    button:SetSize(w,h)
-    button:SetText(text)
-    button:SetTextColor(color)
-    button.DoClick =
-    function()
-      net.Start("f_dbuttonpress")
-        net.WriteEntity(chip)
-        net.WriteString(id)
-      net.SendToServer()
-    end
-
-    DermaID[id] = {}
-    DermaID[id]["parent"] = parent
-    DermaID[id]["obj"] = button
-
-  elseif typ == "textentry" then
-
-    if DermaID[id] then return end
-
-    local x, y, w, h = tab["x"], tab["y"], tab["w"], tab["h"]
-    local text = tab["text"]
-    local parent = tab["parent"]
-
-    local textbox = vgui.Create("DTextEntry", DermaFrames[parent])
-    textbox:SetPos(x,y)
-    textbox:SetSize(w,h)
-    textbox:SetText(text)
-    textbox.OnEnter =
-    function(self)
-      net.Start("f_dtextentry")
-        net.WriteEntity(chip)
-        net.WriteString(id)
-        net.WriteString(self:GetValue())
-      net.SendToServer()
-    end
-
-    DermaID[id] = {}
-    DermaID[id]["parent"] = parent
-    DermaID[id]["obj"] = textbox
-
+    
+    local t = vgui.Create("DLabel", _DermaCore["derma"][parent]["obj"])
+    t:SetPos(pos[1] ,pos[2])
+    t:SetText(text)
+    t:SizeToContents()
+    t:SetTextColor(Color(color[1], color[2], color[3]))
+    
+    _DermaCore["derma"][id] = {}
+    _DermaCore["derma"][id]["parent"] = parent
+    _DermaCore["derma"][id]["obj"] = t
+      
+    -- TODO: Add in DButton, DTextEntry, and more
+    
   end
 
 concommand.Add("de2_clear", function(ply,com,args)
@@ -260,38 +195,10 @@ end)
 
 net.Receive("f_paint", function()
   local tab = net.ReadTable()
-  local x, y, w, h = tab["x"], tab["y"], tab["w"], tab["h"]
-  local id, typ = tab["id"], tab["type"]
-  local color = tab["color"]
-
-  if not DermaID[id] then return end
-
-  if not DermaID[id]["paint"] then DermaID[id]["paint"] = {} end
-  local drawid = table.Count(DermaID[id]["paint"])+1
-
-
-  local tab = {}
-  tab["x"] = x
-  tab["y"] = y
-  tab["w"] = w
-  tab["h"] = h
-  tab["typ"] = typ
-  tab["color"] = color
-  DermaID[id]["paint"][drawid] = tab
-
-  local obj = DermaID[id]["obj"]
-  obj.Paint =
-  function()
-    for k, v in pairs(DermaID[id]["paint"]) do
-      if v["typ"] == "roundedbox" then
-        draw.RoundedBox(0, v["x"], v["y"], v["w"], v["h"], v["color"])
-      end
-    end
-  end
-
+  -- TODO: Remake Paint... again
 end)
 
-concommand.Add("de2_resetall", function(ply,com,args)
-  _DRAllowed = {}
+concommand.Add("de2_resetall", function(ply, com, args)
+  _DermaCore["requests"]["instances"] = {}
   print("Success! Players no longer have access to create Derma on your screen.")
 end)

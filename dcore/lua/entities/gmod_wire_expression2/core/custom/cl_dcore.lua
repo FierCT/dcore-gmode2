537a91bf3f22e0a5f9233dbe304b306943aa39bf
FierDaGamerZ <fierthegamer@gmail.com>
2018-02-09 13:44:52 -0500

    Update cl_dcore.lua
    Removed descriptions of 'return' functions.

--- dcore/lua/entities/gmod_wire_expression2/core/custom/cl_dcore.lua
+++ dcore/lua/entities/gmod_wire_expression2/core/custom/cl_dcore.lua
@@ -6,18 +6,3 @@ E2Helper.Descriptions["dermaFrame"] = "Draw a DFrame on a player's screen."
 E2Helper.Descriptions["dermaText"] = "Draw a DLabel on a player's screen. Parented to a frame."
 E2Helper.Descriptions["dermaButton"] = "Draw a DButton on a player's screen. Parented to a frame. Accepts input."
 E2Helper.Descriptions["dermaTextEntry"] = "Draw a DTextEntry on a player's screen. Parented to a frame. Accepts input."
-
--- User is allowed to have Derma functions
-E2Helper.Descriptions["canRunDerma"] = "Returns if the player is allowing you to draw Derma on their screen."
-E2Helper.Descriptions["dermaAcceptClk"] = "Not necessary due to canRunDerma, but is here regardless."
-
--- Button return functions
-E2Helper.Descriptions["dermaButtonClk"] = "Returns whether or not a button was pressed on the Derma."
-E2Helper.Descriptions["dermaButtonClkPly"] = "Returns the player who pressed a button."
-E2Helper.Descriptions["dermaButtonClkID"] = "Returns the ID (index) of the button that was pressed."
-
--- TextEntry return functions
-E2Helper.Descriptions["dermaTextBoxClk"] = "Returns whether or not a player pressed enter while focused on a DTextEntry."
-E2Helper.Descriptions["dermaTextBoxClkPly"] = "Returns what player pressed enter while focused on a DTextEntry."
-E2Helper.Descriptions["dermaTextBoxClkID"] = "Returns the ID (index) of the DTextEntry that was used."
-E2Helper.Descriptions["dermaTextBoxClkValue"] = "Returns the string that was entered into the DTextEntry."
